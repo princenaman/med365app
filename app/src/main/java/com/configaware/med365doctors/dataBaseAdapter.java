ddfabc0d3183e6e83bbcc02b5c0cb16412dc5bfb
Naman Khator <namankhator@gmail.com>
2015-05-05 02:36:12 +0530

    Updated dataBaseAdapter.java working!!!! Error was inserting values with same id "1" Made autoincrement :)

--- app/src/main/java/com/configaware/med365doctors/dataBaseAdapter.java
+++ app/src/main/java/com/configaware/med365doctors/dataBaseAdapter.java
@@ -2,9 +2,8 @@ package com.configaware.med365doctors;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteException;
 import android.util.Log;
-
 
 
 /**
@@ -20,14 +19,18 @@ public class dataBaseAdapter {
         this.vPassword=vPassword;
         this.vIP=vIP;
         this.vHospital=vHospital;
-        myDatabase=context.openOrCreateDatabase("MED365.db",Context.MODE_PRIVATE,null);
-        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS doctors (_id int, doctor_name text,doctor_password text,doctor_hospital text, doctor_ip text, primary key (_id)) ");
-
+        Log.e("Try","Create Table");
+        try {
+            myDatabase = context.openOrCreateDatabase("MED365.db", Context.MODE_PRIVATE, null);
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS doctors (_id INTEGER PRIMARY KEY AUTOINCREMENT, doctor_name VARCHAR(50), doctor_password  VARCHAR(100), doctor_hospital  VARCHAR(100), doctor_ip  VARCHAR(50))");
+        }catch (SQLiteException e){
+            Log.e("Error",""+e.toString());
+        }
     }
      public void insertData()
      {
 
-         myDatabase.execSQL("INSERT INTO doctors (_id , doctor_name,doctor_password ,doctor_hospital, doctor_ip) values (1,'"+ vName + "','" + vPassword + "','" + vIP + "','" + vHospital+ "')");
+         myDatabase.execSQL("INSERT INTO doctors (doctor_name,doctor_password ,doctor_hospital, doctor_ip) values ('"+ vName + "','" + vPassword + "','" + vIP + "','" + vHospital+ "')");
          Log.e("DB","Inserted Values:"+vName+", "+vPassword+", "+vIP+", "+vHospital);
      }
    /* public void fetchData()
