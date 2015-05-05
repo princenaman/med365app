c6ec98b550e2bf272068aa63aa495ec549fcde75
Naman Khator <namankhator@gmail.com>
2015-05-05 13:36:17 +0530

    Minor changes don't shit me!!!

--- app/src/main/java/com/configaware/med365doctors/HospitalData.java
+++ app/src/main/java/com/configaware/med365doctors/HospitalData.java
@@ -128,7 +128,19 @@ public class HospitalData extends ActionBarActivity implements ActionBar.TabList
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            //return PlaceholderFragment.newInstance(position + 1);
+
+            switch (position) {
+                case 0:
+                    return new FragmentPassword();
+                case 1:
+                    return new FragmentPicture();
+                case 2:
+                    return new FragmentSettings();
+
+            }
+            return null;
+
         }
 
         @Override
